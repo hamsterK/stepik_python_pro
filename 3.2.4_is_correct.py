793df40692473311af8483128795052a9efdbedb
Anastasiya Krauchuk <ext-anastasiya.krauchuk@flowbird.group>
2023-04-02 15:37:28 +0200

    remove my_date

--- 3.2.4_is_correct.py
+++ 3.2.4_is_correct.py
@@ -13,7 +13,7 @@ from datetime import date
 
 def is_correct(day, month, year):
     try:
-        my_date = date(year, month, day)
+        date(year, month, day)
         return True
     except ValueError:
         return False
